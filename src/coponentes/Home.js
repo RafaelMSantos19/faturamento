1b79074039d505bc3dab90fb795aaffcc80c2091
Rafael Santos <rafaelhmsantos19@gmail.com>
2023-01-11 01:41:14 -0300

    docs: create TODO list

--- src/coponentes/Home.js
+++ src/coponentes/Home.js
@@ -2,8 +2,16 @@
 import '../css/Home.css';
 
 export default function Home() {
+
+  //TODO cria coponete com sistema de navegação
+  //TODO cria sistema para navegar entre paginas
+  //TODO cria pagina para inserir os dados(post)
+  //TODO cria pagina para mostra a lista de dados
+  //TODO cria pagina para mudar algum dado ja incerido
+
   return (
-    <div>
+
+    <div> 
         <h1>Ola mundo</h1>
     </div>
   );
